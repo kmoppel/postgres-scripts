806940832f1260cb261a8691fdf78e55102040f7
Kaarel Moppel <kaarel.moppel@gmail.com>
2023-01-06 15:21:37 +0200

    quick_verify_threads.py: Python v2 -> v3

--- quick_verify_threads.py
+++ quick_verify_threads.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import argparse
 import os
@@ -8,7 +8,7 @@ import psycopg2.extras
 import time
 import logging
 import threading
-from Queue import Queue
+from queue import Queue
 from multiprocessing import cpu_count
 args = None
 workers = []
@@ -16,7 +16,7 @@ queue = Queue()
 err_count = 0
 
 
-def executeOnDB(sql, params=None, dbname='postgres'):
+def execute_on_db(sql, params=None, dbname='postgres'):
     conn = psycopg2.connect(host=args.host, port=args.port, dbname=dbname, user=args.username)
     conn.autocommit = True
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
@@ -25,6 +25,7 @@ def executeOnDB(sql, params=None, dbname='postgres'):
         return cur.fetchall()
     else:
         return [{'rows_affected': str(cur.rowcount)}]
+
 
 def shell_exec_with_output(commands, ok_code=0):
     process = subprocess.Popen(commands, stdout=subprocess.PIPE, shell=True, stderr=subprocess.STDOUT)
@@ -35,7 +36,8 @@ def shell_exec_with_output(commands, ok_code=0):
         logging.error(output)
     return exitcode, output
 
-def workerProcess(id):
+
+def worker_process(id):
     """Takes tables from the queue if any, and pg_dumps the table to /dev/null"""
     logging.info('Starting worker process %s ...', id)
     while True:
@@ -54,17 +56,17 @@ def workerProcess(id):
             return
 
 
-def launchWorkers():
+def launch_workers():
     logging.info('Launching %s worker processes', args.jobs)
 
     for i in range(args.jobs):
-        t = threading.Thread(target=workerProcess, args=(i,))
+        t = threading.Thread(target=worker_process, args=(i,))
         t.daemon = True
         t.start()
         workers.append(t)
 
 
-def verifyGlobals():
+def verify_globals():
     logging.info('Dumping globals with pg_dumpall...')
     cmd = '{pg_dumpall} -g -h {host} -p {port} -U "{user}" >/dev/null'.format(
             pg_dumpall=os.path.join(args.bindir, 'pg_dumpall'), host=args.host, port=args.port, user=args.username)
@@ -74,7 +76,7 @@ def verifyGlobals():
         raise Exception('Failed to verify globals')
 
 
-def verifySchema(dbname):
+def verify_schema(dbname):
     logging.info('Dumping schema with pg_dump --schema-only for %s', dbname)
     cmd = '{pg_dump} -h {host} -p {port} -U "{user}" --schema-only "{dbname}" >/dev/null'.format(
             pg_dump=os.path.join(args.bindir, 'pg_dump'), host=args.host, port=args.port, user=args.username, dbname=dbname)
@@ -84,7 +86,7 @@ def verifySchema(dbname):
         raise Exception('Failed to verify schema for DB ' + dbname)
 
 
-def addTablesFromDB(dbname):
+def add_tables_from_db(dbname):
     logging.info('Processing DB: %s', dbname)
     sql = """select quote_ident(nspname)||'.'||quote_ident(relname) as tbl
                 from pg_class c
@@ -93,7 +95,7 @@ def addTablesFromDB(dbname):
                 and relpersistence = 'p'
                 and not nspname like any(array['information_schema', E'pg\\_%'])
                 order by relpages desc"""
-    tables = executeOnDB(sql, dbname=dbname)
+    tables = execute_on_db(sql, dbname=dbname)
     tables = [x['tbl'] for x in tables]
     if tables:
         for t in tables:
@@ -104,7 +106,7 @@ def addTablesFromDB(dbname):
     return len(tables)
 
 
-def allWorkersAlive():
+def all_workers_alive():
     for w in workers:
         if not w.is_alive():
             return False
@@ -118,12 +120,12 @@ def main():
 
     argp.add_argument('--help', help='Show help', action='help')
     argp.add_argument('-b', '--bindir', help='Postgres binaries folder', required=True)
-    argp.add_argument('-h', '--host', help='PG host. IP or unix socket', required=True)
+    argp.add_argument('-h', '--host', help='PG host. IP or unix socket', default='/var/run/postgresql/')
     argp.add_argument('-p', '--port', help='PG port', default=5432, type=int)
     argp.add_argument('-U', '--username', help='PG user', default=os.getenv('USER'))    # password is assumed to be in .pgpass
 
-    argp.add_argument('-j', '--jobs', help='Max parallel processes to use. Default is count of CPUs/2',
-                      default=max(cpu_count()/2, 1), type=int, metavar=max(cpu_count()/2, 1))
+    argp.add_argument('-j', '--jobs', help='Max parallel processes to use. Default is count of CPUs/4',
+                      default=int(max(cpu_count()/4, 1)), type=int, metavar=max(cpu_count()/4, 1))
     argp.add_argument('-d', '--dbname', help='Test only a single DB')
     argp.add_argument('-q', '--quiet', help='Only errors', action='store_true')
 
@@ -136,7 +138,7 @@ def main():
     if not os.path.exists(os.path.join(args.bindir, 'pg_dumpall')):
         raise Exception('Invalid BINDIR! Could not find pg_dumpall')
 
-    dbs = executeOnDB('select datname from pg_database where not datistemplate and datallowconn order by datname', dbname='template1')
+    dbs = execute_on_db('select datname from pg_database where not datistemplate and datallowconn order by datname', dbname='template1')
     dbs = [x['datname'] for x in dbs]
     logging.info('dbs found: %s', dbs)
 
@@ -146,15 +148,15 @@ def main():
         dbs = [args.dbname]
 
     if not args.dbname:
-        verifyGlobals()
+        verify_globals()
 
-    launchWorkers()
+    launch_workers()
 
     tables_added = 0
 
     for db in dbs:
-        verifySchema(db)
-        table_count = addTablesFromDB(db)
+        verify_schema(db)
+        table_count = add_tables_from_db(db)
         tables_added += table_count
 
     if tables_added == 0:
@@ -163,7 +165,7 @@ def main():
     logging.info('Waiting for %s tables to be pg_dumped...', tables_added)
     i = 0
     while not queue.empty():
-        if not allWorkersAlive():
+        if not all_workers_alive():
             logging.error('Not all worker processes are alive. Exiting')
             exit(1)
         time.sleep(5)
@@ -176,6 +178,7 @@ def main():
     else:
         logging.info("Errors encountered")
         exit(1)
+
 
 if __name__ == '__main__':
     main()
